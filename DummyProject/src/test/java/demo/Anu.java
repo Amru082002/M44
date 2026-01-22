6c922f117bea86fec223c7dca5c14ac0e69d82b2
amruta <raskondaamruta32@gamil.com>
2026-01-22 11:54:04 +0530

    commit

--- DummyProject/src/test/java/demo/Anu.java
+++ DummyProject/src/test/java/demo/Anu.java
@@ -5,6 +5,6 @@ public class Anu {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("anushka programs ");
+		System.out.println("hello");
 	}
-
 }
